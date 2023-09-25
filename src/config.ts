2b4e7586a150f7afa36c34c521cf8aed83114a47
Remo <81964236+remoremote@users.noreply.github.com>
2023-09-25 11:44:44 +0200

    initial blog changes

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://maison.work/en",
+  author: "Remo Stahl",
+  desc: "A place where I can publish what I wrote.",
+  title: "remoremote",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 12,
 };
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
@@ -22,7 +22,7 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/remoremote",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
@@ -30,17 +30,17 @@ export const SOCIALS: SocialObjects = [
     name: "Facebook",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Facebook`,
-    active: true,
+    active: false,
   },
   {
     name: "Instagram",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://www.instagram.com/remoremote/",
     linkTitle: `${SITE.title} on Instagram`,
     active: true,
   },
   {
     name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://www.linkedin.com/in/remo-stahl/",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: true,
   },
